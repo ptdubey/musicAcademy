38cf28557ab3d5e5ef25fa8e10c773ef5ffbaa6d
ptdubey <adityababa@gmail.com>
2024-07-23 15:21:35 +0530

    New Changes 07/23/2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     
-    <div className="text-2xl text-center"></div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+   <HeroSection></HeroSection>
+   <FeaturedCourses></FeaturedCourses>
+   </main>
   );
 }
